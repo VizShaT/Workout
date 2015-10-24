528cbd97d91fda699c268bf0f23824e6d0e0561b
Kikit <kikit@users.noreply.github.com>
2015-10-24 20:54:51 +0530

    Working Nested stopwatch

--- app/src/main/java/kikit/workout/StopwatchFragment.java
+++ app/src/main/java/kikit/workout/StopwatchFragment.java
@@ -7,9 +7,10 @@ import android.os.Handler;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
-public class StopwatchFragment extends Fragment {
+public class StopwatchFragment extends Fragment implements View.OnClickListener{
     private int seconds = 0;
     private boolean running;
     private boolean wasRunning;
@@ -36,7 +37,27 @@ public class StopwatchFragment extends Fragment {
         // Inflate the layout for this fragment
         View layout = inflater.inflate(R.layout.fragment_stopwatch, container, false);
         runTimer(layout);
+        Button startButton = (Button)layout.findViewById(R.id.start_button);
+        startButton.setOnClickListener(this);
+        Button stopButton = (Button)layout.findViewById(R.id.stop_button);
+        stopButton.setOnClickListener(this);
+        Button resetButton = (Button)layout.findViewById(R.id.reset_button);
+        resetButton.setOnClickListener(this);
         return layout;
+    }
+    @Override
+    public void onClick(View v){
+        switch (v.getId()){
+            case R.id.start_button:
+                onClickStart(v);
+                break;
+            case R.id.stop_button:
+                onClickStop(v);
+                break;
+            case R.id.reset_button:
+                onClickReset(v);
+                break;
+        }
     }
 
     @Override
