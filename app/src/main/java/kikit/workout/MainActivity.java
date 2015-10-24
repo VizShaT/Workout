9541a128819a460b69af03bdde0322e5d64e4cbb
Kikit <kikit@users.noreply.github.com>
2015-10-24 16:52:35 +0530

    Differnt layout for phone and tablet

--- app/src/main/java/kikit/workout/MainActivity.java
+++ app/src/main/java/kikit/workout/MainActivity.java
@@ -3,7 +3,9 @@ package kikit.workout;
 import android.app.Activity;
 import android.app.Fragment;
 import android.app.FragmentTransaction;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends Activity implements WorkoutListFragment.WorkoutListListener {
 
@@ -13,12 +15,20 @@ public class MainActivity extends Activity implements WorkoutListFragment.Workou
         setContentView(R.layout.activity_main);
     }
     public void itemClicked(long id){
-        WorkoutDetailFragment details = new WorkoutDetailFragment();
-        FragmentTransaction ft = getFragmentManager().beginTransaction();
-        details.setWorkout(id);
-        ft.replace(R.id.fragment_container, details);
-        ft.addToBackStack(null);
-        ft.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
-        ft.commit();
+        View fragmentContainer = findViewById(R.id.fragment_container);
+        if(fragmentContainer != null){
+            WorkoutDetailFragment details = new WorkoutDetailFragment();
+            FragmentTransaction ft = getFragmentManager().beginTransaction();
+            details.setWorkout(id);
+            ft.replace(R.id.fragment_container, details);
+            ft.addToBackStack(null);
+            ft.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
+            ft.commit();
+        }else{
+            Intent intent = new Intent(this, DetailActivity.class);
+            intent.putExtra(DetailActivity.EXTRA_WORKOUT_ID, (int)id);
+            startActivity(intent);
+        }
+
     }
 }
